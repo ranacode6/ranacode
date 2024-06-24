7e73312e2d46f4adf611d2d861759221e21eefee
rana <rana.ahammed.012@gmail.com>
2024-06-24 11:55:41 +0600

    2nd commit

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between w-100% h-[60px] bg-slate-800 shadow-xl px-32">
+      <div>Hello</div>
+      <div>World</div>
+      <div>Rana</div>
+    </header>
+  );
+}
