0814988bd627466d731abb705eabf064b74476d8
rana <rana.ahammed.012@gmail.com>
2024-06-29 09:08:03 +0600

    daisy ui sun icon and moon icon added

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <main className="flex w-full h-full bg-slate-700 flex-col items-center justify-between">
+    <main className="flex w-full h-full bg-slate-700 flex-col">
       <Hero />
     </main>
   );
